1a1a3733c075f4883005c2b77ab3119c7084cede
richardhoran07 <147435783+richardhoran07@users.noreply.github.com>
2025-09-13 21:52:01 -0500

    Create sources.js

---
+++ void/sources.js
@@ -0,0 +1,26 @@
+const SOURCES = [
+  // 🎶 Music / Chill
+  "https://www.youtube.com/embed/5qap5aO4i9A?autoplay=1&mute=1",
+  "https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1&mute=1",
+
+  // 📺 Cartoons / Clips
+  "https://www.youtube.com/embed/IUN664s7N-c?autoplay=1&mute=1",
+  "https://www.youtube.com/embed/3JP9FYP3Vxk?autoplay=1&mute=1",
+
+  // 🍜 Mukbang
+  "https://www.youtube.com/embed/x79XXtT5s5o?autoplay=1&mute=1",
+
+  // 🕹 Gameplay
+  "https://www.youtube.com/embed/8lM2P6t3UX4?autoplay=1&mute=1",
+  "https://www.youtube.com/embed/0j6zGf3zK1E?autoplay=1&mute=1",
+
+  // ⚔️ War / Archival
+  "https://www.youtube.com/embed/8ZcmTl_1ER8?autoplay=1&mute=1",
+  "https://www.youtube.com/embed/qyJdH6glVlw?autoplay=1&mute=1",
+
+  // 📹 CCTV / Reality vibe
+  "https://www.youtube.com/embed/9Auq9mYxFEE?autoplay=1&mute=1", // BBC Live
+  "https://www.youtube.com/embed/21X5lGlDOfg?autoplay=1&mute=1", // NASA live
+
+  // repeat / expand with hundreds more...
+];
